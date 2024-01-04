111a89402d862d1d2469ca4bbf52b3224dc8d962
SahanDev00 <dumidus86@gmail.com>
2024-01-04 23:35:48 +0530

    Updated Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -4,13 +4,14 @@ import {FaSearch} from 'react-icons/fa'
 const Hero = () => {
     return (
         <div className='w-full h-screen'>
-            <img className='top-0 left-0 w-full bg-cover h-screen object-cover ' src="https://img.freepik.com/free-photo/travel-concept-with-landmarks_23-2149153256.jpg?w=740&t=st=1704127087~exp=1704127687~hmac=247e7325fd2fd2002e57e9d5b110efe37640b1ec06bcdbe5e3fdcd8113ef5d81" alt="" />
+            <img className='top-0 left-0 w-full bg-cover h-screen object-cover' src="https://img.freepik.com/free-photo/beautiful-girl-standing-boat-looking-mountains-ratchaprapha-dam-khao-sok-national-park-surat-thani-province-thailand_335224-849.jpg?w=996&t=st=1704361862~exp=1704362462~hmac=3d565e7931ab44a3549c1dece7bf182ad13d4fd85d1769da41171b136171c874" alt="" />
+            <div className="bg-black/40 absolute top-0 left-0 w-full h-screen"/>
             <div className='absolute top-0 flex flex-col w-full h-full justify-center pl-6'>
                 <div className='absolute max-w-[1100px] m-auto p-4 md:left-[10%]'>
-                    <h1 className='text-4xl sm:text-5xl font-bold text-red-600 drop-shadow-lg'>Travel Guide</h1>
+                    <h1 className='text-4xl sm:text-5xl font-bold text-green-500 drop-shadow-lg'>Travel Guide</h1>
                     <p className='text-2xl text-white drop-shadow-lg sm:text-3xl py-4'>Know about your next destination.</p>
                     <input className='h-10 w-80 border-4 border-red-500 p-4' type="text" placeholder='Where would you like to go?' />
-                    <button className='p-4 text-red-600 '><FaSearch /></button>
+                    <button className='p-3 m-1 bg-white text-red-600 border border-red-500 rounded-full'><FaSearch /></button>
                 </div>
             </div>
         </div>
